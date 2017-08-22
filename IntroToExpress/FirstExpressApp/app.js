bc7baa1cae20f11176f6fa93da983b26b1025c67
Jackson Fischer <jfischer7722@gmail.com>
2017-08-22 00:11:46 +0000

    more express

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -17,6 +17,21 @@ app.get("/dog", function(req, res){
     res.send("meow");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("welcome to the " + subreddit + " subreddit");
+})
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+    console.log(req.params);
+    res.send("welcome to the comments page");
+})
+
+
+app.get("*", function(req, res) {
+    res.send("youre a star");
+})
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server has started");
