476506131c51f2da8b00041dde873377989f78bf
Jackson Fischer <jfischer7722@gmail.com>
2017-08-24 00:10:51 +0000

    used control flow ejs

--- MoreExpress/EJSdemo/app.js
+++ MoreExpress/EJSdemo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+            {title: "post 1", author: "suzy"},
+            {title: "cat", author: "matt"},
+            {title: "the eclipse was cool", author: "jack"},
+        ];
+        
+        res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server has started");
 });
