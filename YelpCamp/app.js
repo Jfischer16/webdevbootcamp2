70b960607a43756b25df6bf826e39b2230f73416
Jackson Fischer <jfischer7722@gmail.com>
2017-09-14 23:57:15 +0000

    styled pages with bootstrap including navbar

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -6,9 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-     {name: "salmon creek", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"},
-     {name: "granite hill", image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg"}, 
-     {name: "mountain goats pass", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"} 
+     {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"},
+     {name: "Granite Hill", image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg"}, 
+     {name: "Mountain Goats Pass", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"} 
 ];
 
 app.get("/", function(req, res){
