d82c41a80b3e890cdbb028c9e37903b0a49353ff
Jackson Fischer <jfischer7722@gmail.com>
2017-11-10 02:49:44 +0000

    finished adding basic authentication and link hiding

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -29,6 +29,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -40,7 +45,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-          res.render("campgrounds/index",{campgrounds:allCampgrounds});
+          res.render("campgrounds/index",{campgrounds:allCampgrounds, currentUser: req.user});
        }
     });
 });
@@ -86,7 +91,7 @@ app.get("/campgrounds/:id", function(req, res){
 //COMMENTS ROUTES
 //===============
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Campground.findById(req.params.id, function(err, campground){
       if(err){
@@ -97,7 +102,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
    //lookup campground using id
    Campground.findById(req.params.id, function(err, campground){
       if(err){
@@ -152,6 +157,19 @@ app.post("/login", passport.authenticate("local",
         failureRedirect: "/login"
      }), function(req, res){
 });
+
+// logic route
+app.get("/logout", function(req, res){
+   req.logout(); 
+   res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
