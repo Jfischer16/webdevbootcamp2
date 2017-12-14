f3884901ecab44bb459402acfaa819b5e5dd07b3
Jackson Fischer <jfischer7722@gmail.com>
2017-12-14 01:45:34 +0000

    now only signed in users can edit and delete

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -64,7 +64,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 });
 
 //update campground route
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
    //find and update the correct campground
    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
        if(err){
@@ -76,7 +76,7 @@ router.put("/:id", function(req, res){
 });
 
 //destroy campground route
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
    Campground.findByIdAndRemove(req.params.id, function(err){
        if(err){
            res.redirect("/campgrounds");
