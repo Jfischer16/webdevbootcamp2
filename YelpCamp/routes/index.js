ac8a6300f06ce7842250b09fd2f90ce00a17d425
Jackson Fischer <jfischer7722@gmail.com>
2018-01-17 02:12:44 +0000

    added more error messages to improve ui

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -16,11 +16,12 @@ router.get("/register", function(req, res){
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render("register");
-        }
+         if(err){
+           req.flash("error", err.message);
+           return res.redirect("/register");
+       }
         passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Welcome to YelpCamp " + user.username);
             res.redirect("/campgrounds");
         });
     });
@@ -45,12 +46,5 @@ router.get("/logout", function(req, res){
    res.redirect("/campgrounds");
 });
 
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
 
 module.exports = router;
